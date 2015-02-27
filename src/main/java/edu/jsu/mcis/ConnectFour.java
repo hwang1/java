1cfd4bd1ebfd3cbd700ee1ae30f56baf24d59b7b
hwang1 <hwang1@jsu.edu>
2015-02-27 12:17:08 -0600

    Update ConnectFour.java

--- src/main/java/edu/jsu/mcis/ConnectFour.java
+++ src/main/java/edu/jsu/mcis/ConnectFour.java
@@ -1,10 +1,17 @@
 package edu.jsu.mcis;
+private Location [][]borad;
 
 public class ConnectFour {
     public enum Location {EMPTY, RED, BLACK};
     
     public ConnectFour() {
-        
+        borad = new Location [ROWS][COLUMN]
+		for (int i=0; i < borad.length; i++){
+			for(int j=0;j<borad[i].length;i++){
+				borad[i][j] Location.EMPTY;
+			}
+		}
+		return true;
     }
     
     public Location getTopOfColumn(int column) {
